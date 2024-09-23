b057f71b65a1fb519ee0ffab6e6fc35c429e51ad
Muhammad Difa Sulthon Diani <mdifasulthon@gmail.com>
2024-09-23 22:21:01 +0700

    feat: add custom color and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1F3E97',
+        secondary: '#18378F',
+        grayPrimary: '#858585',
+        graySecondary: '#F4F4F4'
+      },
+      fontFamily: {
+        logoText: ["Pacifico"]
+      }
+    },
   },
   plugins: [],
 }
