ce03c2bc5386e9c86c2281616917b1042d41ee20
Muhammad Difa Sulthon Diani <mdifasulthon@gmail.com>
2024-09-24 22:01:05 +0700

    feat: add input search in home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Search from "../../assets/icons/search.svg";
+import InputSearch from "../../components/InputSearch";
 
 const Home = (): React.ReactElement => {
-  return <div>Home</div>;
+  const [keyWord, setKeyWord] = useState("");
+
+  const handleSearchChange = (value: string) => {
+    setKeyWord(value);
+  };
+  return (
+    <div className="flex">
+      <div className="flex flex-grow justify-center mt-6">
+        <InputSearch
+          onChange={(e) => handleSearchChange(e.target.value)}
+          value={keyWord}
+          type="text"
+          placeholder="Search..."
+          isFullRounded
+          rightIcon={<img src={Search} alt="Search" className="w-4 h-4" />}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
